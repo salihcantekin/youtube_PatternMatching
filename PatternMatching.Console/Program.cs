18fd6d984b32c9e3d109cb05a571ee6b328a763b
Salih Cantekin <salihcantekin@gmail.com>
2022-06-23 20:36:55 +0100

    Add project files.

---
+++ PatternMatching.Console/Program.cs
@@ -0,0 +1,96 @@
+﻿
+
+using PatternMatching.Console;
+
+var studentList = new List<Student>()
+{
+    new ITStudent(new Random().Next(100)) {  ComputerExamResult = 29},
+    new MISStudent(new Random().Next(100))
+};
+
+var student = studentList[new Random().Next(studentList.Count)];
+
+//if (student is ITStudent)
+//{
+//    ITStudent itStudent = (ITStudent)student;
+
+//    Console.WriteLine("FullName: {0}", itStudent.ExamResult);
+//}
+
+//if (student is ITStudent itStudent)
+//{
+//    Console.WriteLine("FullName: {0}", itStudent.FullName);
+//}
+
+
+//if (student is ITStudent itStudent)
+//{
+//    if (itStudent.ComputerExamResult > 25 && itStudent.ComputerExamResult < 50)
+//    {
+//        Console.WriteLine("Between 25 and 50");
+//    }
+//    else
+//    {
+//        Console.WriteLine("NOT BETWEEN. {0}", itStudent.ComputerExamResult);
+//    }
+//}
+
+//if (student is ITStudent { ComputerExamResult: > 25 and < 50 } itStudent)
+//{
+//    Console.WriteLine("Between 25 and 50");
+//}
+//else
+//    Console.WriteLine("NOT BETWEEN.");
+
+
+//if (student is ITStudent || student is not MISStudent { ProjectManagementExamResult: > 30 })
+//{
+//    Console.WriteLine(student.GetType().FullName);
+
+//    if (student is MISStudent mis)
+//    {
+//        Console.WriteLine("Result: {0}", mis.ProjectManagementExamResult);
+//    }
+//}
+
+
+
+
+//switch (student)
+//{
+//    case ITStudent itStudent:
+//        {
+//            Console.WriteLine("FullName: {0}", itStudent.FullName);
+//            Console.WriteLine("ExamResult: {0}", itStudent.ExamResult);
+//            Console.WriteLine("ComputerExamResult: {0}", itStudent.ComputerExamResult);
+//            break;
+//        }
+//        break;
+//    case MISStudent misStudent:
+//        Console.WriteLine();
+//        break;
+//    default:
+//        break;
+//}
+
+
+var alumni = student switch
+{
+    ITStudent itStudent => itStudent.ComputerExamResult > 49,
+    MISStudent misStudent => misStudent.ProjectManagementExamResult > 69,
+    Student s => s.ExamResult > 29,
+    _ => false
+};
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("\n\nDONE!");
+Console.ReadKey();
---
+++ PatternMatching.Console/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternMatching.Console;
+public abstract class Student
+{
+    public string FullName { get; set; } = "Salih Cantekin";
+
+    public int ExamResult { get; set; }
+
+    public Student(int examResult)
+    {
+        ExamResult = examResult;
+    }
+}
+
+public class ITStudent : Student
+{
+    public ITStudent(int examResult) : base(examResult)
+    {
+    }
+
+    public int ComputerExamResult { get; set; }
+}
+
+public class MISStudent : Student
+{
+    public MISStudent(int examResult) : base(examResult)
+    {
+    }
+
+    public int ProjectManagementExamResult { get; set; }
+}
